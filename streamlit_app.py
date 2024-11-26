bec6b02a58614823de2cbb2010d56589100f6ba9
jharikrishna <30597027+jharikrishna@users.noreply.github.com>
2024-11-26 20:31:25 +0530

    Update streamlit_app.py
    updated the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,11 @@ if ingredients_list:
    #New section to display smoothiefroot nutrition information
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    #st.text(smoothiefroot_response).json()
+    # Assuming the response is in JSON format, use .json() to parse it
+     smoothie_data = smoothiefroot_response.json()
+    # Display the response in Streamlit as text
+     st.text(smoothie_data)
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
